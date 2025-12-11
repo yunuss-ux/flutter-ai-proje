e2b6c56084c806f1975b24b68dd7af776f762fed
yunuss-ux <ynsmregny@gmail.com>
2025-12-11 18:37:46 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'chat_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter AI Assistant',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      // Uygulama ChatScreen sayfasından başlar
+      home: const ChatScreen(),
+    );
+  }
+}
